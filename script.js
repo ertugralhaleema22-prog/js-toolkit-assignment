04d2b6a2a79ef5d9ac83d67d82af616d5ad0e09f
ertugralhaleema22-prog <ertugralhaleema22@gmail.com>
2026-04-25 20:48:12 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    document.getElementById("output").innerHTML = "Hello! This is my first function.";
+}
+
+function calculateSum() {
+    let a = 10;
+    let b = 20;
+    let sum = a + b;
+
+    document.getElementById("output").innerHTML = "Sum is: " + sum;
+}
